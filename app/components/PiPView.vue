f07660702090eb1dec09bad36b04df258fd0b2bb
Lucas Santiago <lucasfsanti@gmail.com>
2026-07-22 11:56:08 -0300

    feat: add PiPView component for Picture-in-Picture window

--- app/components/PiPView.vue
+++ app/components/PiPView.vue
@@ -1,3 +1,15 @@
 <template>
-  <div />
+  <div class="flex flex-wrap justify-center gap-4 p-4 h-full">
+    <Timer
+      v-for="timer in timers"
+      :key="timer.timerId"
+      :timer-id="timer.timerId"
+      :palette="timer.palette"
+      :show-delete="false"
+    />
+  </div>
 </template>
+
+<script setup lang="ts">
+const { timers } = useTimerList()
+</script>
